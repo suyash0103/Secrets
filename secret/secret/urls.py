2abbeede7c6c6d8722b51c2477eb6b6c1b5871fa
suyash0103 <suyashghuge@gmail.com>
2018-04-04 14:33:20 +0530

    user urls

--- secret/secret/urls.py
+++ secret/secret/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'^user/', include('user.urls')),
 ]
